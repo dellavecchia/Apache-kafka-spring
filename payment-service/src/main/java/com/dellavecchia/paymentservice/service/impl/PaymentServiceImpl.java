082433a143bdfcca6b9953f49ea11f2933dbb456
Gabriel Guimaraes <gabrieldellavecchia@gmail.com>
2023-08-03 09:10:35 -0300

    Add producer config

--- payment-service/src/main/java/com/dellavecchia/paymentservice/service/impl/PaymentServiceImpl.java
+++ payment-service/src/main/java/com/dellavecchia/paymentservice/service/impl/PaymentServiceImpl.java
@@ -2,13 +2,26 @@ package com.dellavecchia.paymentservice.service.impl;
 
 import com.dellavecchia.paymentservice.model.Payment;
 import com.dellavecchia.paymentservice.service.PaymentService;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
 @Log4j2
 @Service
 public class PaymentServiceImpl implements PaymentService {
+
+    private final KafkaTemplate kafkaTemplate;
+
+    @SneakyThrows
     @Override
     public void sendPayment(Payment payment) {
-        log.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+        log.info("Recebi o pagamento {}", payment);
+        Thread.sleep(1000);
+
+        log.info("Enviando pagamento ...");
+        kafkaTemplate.send("payment-topic", payment);
     }
 }
