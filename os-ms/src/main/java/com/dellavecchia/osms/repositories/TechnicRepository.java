d57527453b057e53a17bbecb1be0d51637389180
Gabriel Guimarães <gabrieldellavecchia@gmail.com>
2023-08-15 12:37:31 -0300

    Entities repositories implemented

---
+++ os-ms/src/main/java/com/dellavecchia/osms/repositories/TechnicRepository.java
@@ -0,0 +1,9 @@
+package com.dellavecchia.osms.repositories;
+
+import com.dellavecchia.osms.domain.Technic;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TechnicRepository extends JpaRepository<Technic, Integer> {
+}
