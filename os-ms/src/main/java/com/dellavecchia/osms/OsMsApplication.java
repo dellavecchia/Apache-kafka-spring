49c5c403d012ee70aff6360b24e3260056c06f85
Gabriel Della Vecchia <gabrieldv@Gabriels-MacBook-Pro-16.local>
2023-08-15 11:27:35 -0300

    base project

---
+++ os-ms/src/main/java/com/dellavecchia/osms/OsMsApplication.java
@@ -0,0 +1,13 @@
+package com.dellavecchia.osms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OsMsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OsMsApplication.class, args);
+    }
+
+}
--- os-ms/src/main/java/com/dellavecchia/osms/domain/Technic.java
+++ os-ms/src/main/java/com/dellavecchia/osms/domain/Technic.java
@@ -1,10 +1,30 @@
 package com.dellavecchia.osms.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.experimental.SuperBuilder;
 
+import java.util.ArrayList;
+import java.util.List;
 
+@Entity
 public class Technic extends Person {
+
+    @OneToMany(mappedBy = "technic")
+    private List<OS> list = new ArrayList<>();
+    public Technic() {
+        super();
+    }
+
     public Technic(Integer id, String name, String cpf, String phone) {
         super(id, name, cpf, phone);
+    }
+
+    public List<OS> getList() {
+        return list;
+    }
+
+    public void setList(List<OS> list) {
+        this.list = list;
     }
 }
